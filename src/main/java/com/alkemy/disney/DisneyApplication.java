869bef59c373af18f862040a14d15ae011183351
Erika Farias <erika.aldana.farias@gmail.com>
2022-05-23 14:42:01 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/disney/DisneyApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApplication.class, args);
+	}
+
+}
