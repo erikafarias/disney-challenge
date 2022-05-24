987cf73c18ecd7b06549b8a554af67f1e1f2fe74
Matías Vera <matias_vera@outlook.com>
2022-05-24 08:47:34 -0300

    Genre Entity

--- src/main/java/com/alkemy/disney/entity/GenreEntity.java
+++ src/main/java/com/alkemy/disney/entity/GenreEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "genre")
@@ -14,6 +15,15 @@ public class GenreEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    private String name;
+    private String image;
+
+    @ManyToMany(mappedBy = "genres")
+    private List<MovieEntity> movies;
+
+
+
 
 
 }
