49c37cbac99e5e446926ee0a9e7a3af31e31ff99
Matías Vera <matias_vera@outlook.com>
2022-05-26 11:06:05 -0300

    Added @RequestBody

--- src/main/java/com/alkemy/disney/controller/GenreController.java
+++ src/main/java/com/alkemy/disney/controller/GenreController.java
@@ -4,6 +4,7 @@ import com.alkemy.disney.entity.GenreEntity;
 import com.alkemy.disney.service.impl.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class GenreController {
     private GenreService genreService;
 
     @PostMapping("/genres")
-    public void addGenre(GenreEntity genre) {
+    public void addGenre(@RequestBody GenreEntity genre) {
         genreService.addGenre(genre);
     }
 }
