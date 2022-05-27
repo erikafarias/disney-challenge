3c876d0d994d81fcab85ff2bed5d61c50fcb59de
Matías Vera <matias_vera@outlook.com>
2022-05-27 14:19:44 -0300

    Fixed implementation of service layer in controller layer

--- src/main/java/com/alkemy/disney/controller/GenreController.java
+++ src/main/java/com/alkemy/disney/controller/GenreController.java
@@ -1,8 +1,7 @@
 package com.alkemy.disney.controller;
 
 import com.alkemy.disney.dto.GenreDTO;
-import com.alkemy.disney.entity.GenreEntity;
-import com.alkemy.disney.service.impl.GenreServiceImpl;
+import com.alkemy.disney.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenreController {
 
     @Autowired
-    private GenreServiceImpl genreService;
+    private GenreService genreService;
 
     @PostMapping("/genres")
     public GenreDTO addGenre(@RequestBody GenreDTO genre) {
