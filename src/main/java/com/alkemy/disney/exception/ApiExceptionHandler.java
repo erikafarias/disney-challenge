049ce0a321ca795d6e9e6ec7bd9675801ab91cda
Erika Farias <erika.aldana.farias@gmail.com>
2022-05-31 16:38:37 -0300

    Valid exception message

--- src/main/java/com/alkemy/disney/exception/ApiExceptionHandler.java
+++ src/main/java/com/alkemy/disney/exception/ApiExceptionHandler.java
@@ -1,25 +1,51 @@
 package com.alkemy.disney.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {ApiRequestException.class})
-    public ResponseEntity<Object> handleException(ApiRequestException e) {
+    final HttpStatus BAD_REQUEST = HttpStatus.BAD_REQUEST;
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ArrayList<String> errors = new ArrayList<>();
+        for(FieldError error: ex.getBindingResult().getFieldErrors()){
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        ApiException apiException = new ApiException(
+                ex.getMessage(),
+                BAD_REQUEST ,
+                ZonedDateTime.now(ZoneId.of("Z")),
+                errors);
+        return new ResponseEntity(apiException, BAD_REQUEST);
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleException(RuntimeException e) {
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         ApiException apiException = new ApiException(
                 e.getMessage(),
-                badRequest,
+                BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
 
         return new ResponseEntity<>(apiException, badRequest);
 
     }
+
+
+
 }
